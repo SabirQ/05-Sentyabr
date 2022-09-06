d7080d1a39c36ba506809c6923317c19850b0bb8
SabirQ <sabirmg@code.edu.az>
2022-09-06 06:04:42 +0400

    Task added

---
+++ Task_05_09/Task_05_09/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_05_09
+{
+    public class Country
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string ISO { get; set; }
+        public string Capital { get; set; }
+        public string Currency { get; set; }
+        public string Continent { get; set; }
+
+	}
+
+
+}
